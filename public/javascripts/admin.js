13d8e75481c996cfa284bda68ecbb819dbc7b9e7
kokikode <nanjiro.ko@gmail.com>
2015-05-05 20:04:42 +0800

    layout enhancement

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,8 +1,8 @@
 $(document).on('ready', function(){
 	$('#profileImgDeleteBtn').on('click', function(){
 		console.log('asd')
-		$(this).closest('#userProfileImgEdit').find('img').attr('src', '/profiles/none.jpg');
-		$('input[name="avatar_old"]').val('none.jpg');
+		$(this).closest('#userProfileImgEdit').find('img').attr('src', '/profiles/none.png');
+		$('input[name="avatar_old"]').val('none.png');
 		$(this).addClass('hide');
 	})
 })
